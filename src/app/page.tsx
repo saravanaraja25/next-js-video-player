bd2c5a6a172627a7b8e0f6670c244a926f982df6
Saravana Raja P P <saravanaraja25@gmail.com>
2024-03-14 23:18:55 +0530

    remove container from main div and unused codes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import VideoPlaylist from "@/components/VideoPlaylist";
 
 export default function Home() {
   return (
-    <div className="container min-h-[calc(100vh-104px)]">
-      {/* make me grid with gap-8 that has 2/3 for video container and 1/3 for video list also have a max width so while scrolling it dont shrink and make it each in row if it below lg screen*/}
+    <div className="min-h-[calc(100vh-104px)]">
       <div className="grid grid-cols-3 gap-8">
         <div className="xl:col-span-2 col-span-3">
           <div>
@@ -17,19 +16,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
-      {/* <div className="grid grid-cols-3 gap-8">
-        <div className="xl:col-span-2 col-span-3">
-          <div>
-            <VideoPlayer />
-          </div>
-        </div>
-        <div className="xl:col-span-1 col-span-3">
-          <div>
-            <VideoPlaylist />
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 }
